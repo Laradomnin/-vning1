298644be04e52778ab7e73d8483581425eb8858f
larad <larad@LAPTOP-19SFN444>
2022-05-10 16:37:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace övning1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ditt namn.");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Ange månadslön, " + namn);
+            string lön = Console.ReadLine();
+            Console.WriteLine( lön );
+     
+        }
+    } 
+}
